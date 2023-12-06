f1a886fbdbba74028ed966370ee72f68017b6cd5
yoshio kinoshita <p03-kinoshita@rosic.jp>
2023-12-06 14:06:51 +0900

    add gpt models

--- types/openai.ts
+++ types/openai.ts
@@ -8,19 +8,20 @@ export interface OpenAIModel {
 }
 
 export enum OpenAIModelID {
-  GPT_3_5 = 'gpt-3.5-turbo',
+  GPT_3_5_TURBO = 'gpt-3.5-turbo-1106',
   GPT_3_5_AZ = 'gpt-35-turbo',
   GPT_4 = 'gpt-4',
+  GPT_4_TURBO = 'gpt-4-1106-preview',
   GPT_4_32K = 'gpt-4-32k',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_3_5;
+export const fallbackModelID = OpenAIModelID.GPT_3_5_TURBO;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
-  [OpenAIModelID.GPT_3_5]: {
-    id: OpenAIModelID.GPT_3_5,
-    name: 'GPT-3.5',
+  [OpenAIModelID.GPT_3_5_TURBO]: {
+    id: OpenAIModelID.GPT_3_5_TURBO,
+    name: 'GPT-3.5-TURBO',
     maxLength: 12000,
     tokenLimit: 4000,
   },
@@ -32,9 +33,15 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   },
   [OpenAIModelID.GPT_4]: {
     id: OpenAIModelID.GPT_4,
-    name: 'GPT-4',
+    name: '利用不可 GPT-4',
     maxLength: 24000,
     tokenLimit: 8000,
+  },
+  [OpenAIModelID.GPT_4_TURBO]: {
+    id: OpenAIModelID.GPT_4_TURBO,
+    name: '利用不可 GPT-4-TURBO',
+    maxLength: 128000,
+    tokenLimit: 4096,
   },
   [OpenAIModelID.GPT_4_32K]: {
     id: OpenAIModelID.GPT_4_32K,
